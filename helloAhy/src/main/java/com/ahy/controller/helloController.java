dcd6a7a967b02fae2a7f70b1454b8ee05942f8e1
liyue <liyue@atwasoft.net>
2020-11-27 17:19:07 +0800

    hello,ahy

---
+++ helloAhy/src/main/java/com/ahy/controller/helloController.java
@@ -0,0 +1,21 @@
+package com.ahy.controller;/**
+ * @FileName: helloController
+ * @PACKAGE_NAME com.ahy.controller
+ * @Description: //TODO
+ * @author ahy
+ * @DATE 2020/11/27
+ * @version V1.0
+ */
+
+/**
+ * @Description
+ * @ClassName helloController
+ * @Author ahy
+ * @date 2020.11.27
+ * @Version 1.0
+ */
+public class helloController {
+    public static void main(String[] args) {
+        System.out.print("hello,ahy");
+    }
+}
